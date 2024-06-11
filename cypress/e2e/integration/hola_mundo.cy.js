f785098e27f12fa5735db46d0a3363e786bd20a5
Fernando Verde <fernando.verde98@outlook.com>
2024-06-11 15:01:10 -0500

    demoqaa

--- cypress/e2e/integration/hola_mundo.cy.js
+++ cypress/e2e/integration/hola_mundo.cy.js
@@ -7,7 +7,7 @@ describe('Bienvenido cypress', () => {
         cy.visit("https://demoqa.com/text-box");
         cy.get('#userName').type("fernandi")
         cy.wait(3000)
-        cy.get('#userEmail').type("verde")
+        cy.get('#userEmail').type("verdee")
     });
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false;
