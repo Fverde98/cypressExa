d9570d570079de42ab1bab57edab1affbb362ae7
Fernando Verde <fernando.verde98@outlook.com>
2024-06-20 15:42:19 -0500

    funcion select y reto 3

---
+++ cypress/e2e/integration/section 2/selects.cy.js
@@ -0,0 +1,29 @@
+require('cypress-xpath');
+describe('Nueva seccion checbok', () => {
+    it('check one', () => {
+        cy.visit('https://demoqa.com/select-menu')
+        cy.title().should('eq','DEMOQA')
+        cy.wait(1500)
+        cy.get('#oldSelectMenu').should('be.visible').select('Green').should('have.value','2')
+    });
+    it('check two', () => {
+        cy.visit('https://www.google.com/?hl=es')
+        cy.title().should('eq','Google')
+        cy.wait(1500)
+        cy.get('[name="q"]').should('be.visible').type('ferrari').type('{enter}')
+        cy.get('#jZ2SBf > .wM6W7d > span').click()
+    });
+    it.only('Opcion multi select', () => {
+        cy.visit('https://demoqa.com/select-menu')
+        cy.title().should('eq','DEMOQA')
+        cy.wait(1500)
+        cy.get('[name="cars"]').should('be.visible').select('Saab','Audi').then(()=>{
+         cy.get('#printme').should('be.visible').click()
+        })
+        
+    });
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false;
+       });
+});
+
---
+++ cypress/e2e/integration/section 2/threeChallenge.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+describe('3er challeng', () => {
+    it('exercise 1', () => {
+        cy.visit('https://www.jqueryscript.net/demo/Efficient-Dual-List-Box-Plugin-with-jQuery-Bootstrap/')
+        cy.title().should('eq','jQuery Dual List Box Demo')
+        cy.wait(1500)
+        cy.get('.unselected').should('be.visible').select('Adams Pittman')
+        cy.get('.str').click()
+    });
+});
