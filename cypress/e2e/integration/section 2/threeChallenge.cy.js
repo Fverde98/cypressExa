74b69dc8328a23d1e3f494ba780386b7721bdb2f
Fernando Verde <fernando.verde98@outlook.com>
2024-06-20 16:26:05 -0500

    update

--- cypress/e2e/integration/section 2/threeChallenge.cy.js
+++ cypress/e2e/integration/section 2/threeChallenge.cy.js
@@ -5,6 +5,13 @@ describe('3er challeng', () => {
         cy.title().should('eq','jQuery Dual List Box Demo')
         cy.wait(1500)
         cy.get('.unselected').should('be.visible').select('Adams Pittman')
-        cy.get('.str').click()
+        cy.get('.str').click().then(()=>{
+        cy.wait(1500)   
+        cy.get('.atr').click().then(()=>{
+            cy.get('.atl').click().then(()=>{
+            cy.log('Se movieron todos los elementos')
+            })
+        })  
+        })
     });
 });
